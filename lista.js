c3a71e959c0e611ba918d87e27e344d7101c3bed
Rafaelm <isaizanami@gmail.com>
2024-09-18 16:39:36 -0300

    clear do input, function createButton criado, e criar tarefa com enter implantado

--- lista.js
+++ lista.js
@@ -6,14 +6,35 @@ function createLista(textoInput) {
   const li = criarLi();
   li.innerHTML = textoInput;
   tarefas.appendChild(li);
+  clearInput();
+  createButton(li);
 }
-console.log(createLista());
+
 function criarLi() {
   const li = document.createElement("li");
   return li;
+}
+function createButton(li) {
+  li.innerText += " ";
+  const buttonCreate = document.createElement("button");
+  buttonCreate.innerText = "apagar";
+  buttonCreate.setAttribute("class", "apagar");
+  li.appendChild(buttonCreate);
+}
+
+function clearInput() {
+  inputTarefas.value = "";
+  inputTarefas.focus();
 }
 
 btnTarefas.addEventListener("click", function () {
   if (!inputTarefas.value) return;
   createLista(inputTarefas.value);
+});
+
+inputTarefas.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    if (!inputTarefas.value) return;
+    createLista(inputTarefas.value);
+  }
 });
