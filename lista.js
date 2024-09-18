415f6a9c59307ff1966d46d761284d8158c41d2b
Rafaelm <isaizanami@gmail.com>
2024-09-18 17:54:29 -0300

    comentando as funçoes e funcionalidades dos elementos

--- lista.js
+++ lista.js
@@ -1,7 +1,9 @@
+// criado 3 variaveis que seleciona os conteudos da pagina html
 const inputTarefas = document.querySelector(".input-tarefas");
 const btnTarefas = document.querySelector(".btn-tarefas");
 const tarefas = document.querySelector(".tarefas");
-
+// criado a funçao de criar listas passando como parametro o textoInput e colocando como filho de
+//tarefas que é a classe da tag lu no html
 function createList(textoInput) {
   const li = criarLi();
   li.innerHTML = textoInput;
@@ -10,11 +12,13 @@ function createList(textoInput) {
   createButton(li);
   salvarTarefas();
 }
-
+// funçao que cria os li que seram usados na funçao creatList
 function criarLi() {
   const li = document.createElement("li");
   return li;
 }
+// funçao que cria botoes que seram filhos dos li quando voce cria alguma tarefa e teram a funçao de apagar.
+// e adiciona a classe apagar no botao.
 function createButton(li) {
   li.innerText += " ";
   const buttonCreate = document.createElement("button");
@@ -22,12 +26,12 @@ function createButton(li) {
   buttonCreate.setAttribute("class", "apagar");
   li.appendChild(buttonCreate);
 }
-
+// funçao que limpa o input e da focus
 function clearInput() {
   inputTarefas.value = "";
   inputTarefas.focus();
 }
-
+// funçao que salva as tarefas criadas pelos li e
 function salvarTarefas() {
   const liTarefas = tarefas.querySelectorAll("li");
   const listaTarefas = [];
@@ -37,12 +41,11 @@ function salvarTarefas() {
     tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
     listaTarefas.push(tarefaTexto);
   }
-
+  // Converte o array 'listaTarefas' em uma string JSON
   const tarefaJSON = JSON.stringify(listaTarefas);
   localStorage.setItem("tarefas", tarefaJSON);
-  console.log(tarefaJSON);
 }
-
+// funçao que pega as tarefas salvas no localStorage usando a key 'tarefas' e mantem na pagina ao atualizar
 function addTarefasSalvas() {
   const tarefas = localStorage.getItem("tarefas");
   const listaDeTarefas = JSON.parse(tarefas);
@@ -51,19 +54,19 @@ function addTarefasSalvas() {
     createList(tarefa);
   }
 }
-
+// funcionalidade do botao de criar a tarefa usando o conteudo do input
 btnTarefas.addEventListener("click", function () {
   if (!inputTarefas.value) return;
   createList(inputTarefas.value);
 });
-
+// funcionalidade de criar tarefa ao apetar enter usando o evento keypress e no if o keycode que identifica a tecla enter
 inputTarefas.addEventListener("keypress", function (e) {
   if (e.keyCode === 13) {
     if (!inputTarefas.value) return;
     createList(inputTarefas.value);
   }
 });
-
+// funcionalidade do botao apagar que apaga a tarefa da lista
 document.addEventListener("click", function (e) {
   const el = e.target;
   if (el.classList.contains("apagar")) {
